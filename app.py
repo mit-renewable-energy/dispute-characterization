b5ca3e836fb6f17feef543d7ebed1615439d390b
anushreechaudhuri <anushreec2@gmail.com>
2023-11-02 01:45:58 -0400

    good to go

--- app.py
+++ app.py
@@ -1,22 +1,64 @@
 import streamlit as st
 import pandas as pd
 import json
+import gspread
+import dotenv
+import os
+import pandas as pd
+from streamlit_js_eval import streamlit_js_eval
+
+
+dotenv.load_dotenv()
+
+@st.cache_resource
+def get_gc():
+    """Get gspread client"""
+    with open("gcloud.json", "w") as f:
+        f.write(os.getenv("GOOGLE_API_KEY"))
+    gc = gspread.service_account(filename="gcloud.json")
+    return gc
 
 st.set_page_config(layout="wide")
 
-st.title("Dispute Characterization")
+gc = get_gc()
+sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1NaitOeamRWgkJlj5JCBmyJwwImgC-o2qAeIvlg2w9eI/edit#gid=309366946")
 
-queries = pd.read_csv('query_updated.csv')
+
+db = pd.DataFrame(sh.sheet1.get_all_records())
+
+st.info("""Here is some info you should keep in mind when scoring:
+
+Anushree info goes here
+
+""")
+
+name = st.text_input("What is your name?")
+
+active_row = db[(db['human'].isna()) | (db['human'] == '')].iloc[0]
+
+st.info(f"These are results from the following search query:\n\n {active_row['query']}")
+
+results = {
+
+}
+
+for j, result in enumerate(json.loads(active_row['result'])):
+    f"""
+    ====================================================================================================
+
+    **{result['title'] if "title" in result else "No title available"}**
+    {result['display_link'] if "display_link" in result else "No description available"}
+
+    {result['description'] if "description" in result else "No description available"}
+    """
+
+    results[j] = st.checkbox("Is this relevant? (i.e. would you click this)", key=f"{j}")
 
 
-for i, q in queries.iterrows():
-    with st.expander(q['query']):
-        for j, result in enumerate(json.loads(q['result'])['organic']):
-            f"""
-            #### {result['title']}
-            {result['display_link']}
-
-            {result['description']}
-            """
-
-            st.checkbox("Is this relevant? (i.e. would you click this)", key=f"{i}_{j}")
+if st.button("Submit Results", use_container_width=True):
+    if type(name) == str and name != "":
+        db.loc[active_row.name, 'human'] = json.dumps({"name": name, "submission": results})
+        sh.sheet1.update([db.columns.values.tolist()] + db.values.tolist())
+        streamlit_js_eval(js_expressions="parent.window.location.reload()")
+    else:
+        st.error("Please enter your name and submit again.")
